8fdfc65037bacb5a8b9cbf089167fe57b9befb64
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-09-17 22:16:44 +0400

    Save lastname test file before switching branch

---
+++ tests/registration/lastname.spec.ts
@@ -0,0 +1,15 @@
+// import { expect, test } from '@playwright/test';
+// import { registerPage } from '../../src/pages/registrationPage';
+
+// test.describe('Last name Suite', () =>{
+
+//     test.beforeEach(async({page}) =>{
+//         const register = new registerPage(page);
+//         register.goto();
+//         await register.fillRequiredFieldsExcept('lastname');
+//     })
+
+//     test('[AQAPRACT-514]: Register with max Last name length (255 characters)', async({page}) =>{
+//         const register = new registerPage(page);
+//     })
+// })
