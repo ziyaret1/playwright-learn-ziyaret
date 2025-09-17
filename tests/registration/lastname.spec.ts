21e992714440972ee85ceb7e69ee5bb638c4b516
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-09-17 21:56:34 +0400

    Firstname suite automated

--- tests/registration/lastname.spec.ts
+++ tests/registration/lastname.spec.ts
@@ -4,6 +4,12 @@ import { registerPage } from '../../src/pages/registrationPage';
 test.describe('Last name Suite', () =>{
 
     test.beforeEach(async({page}) =>{
+        const register = new registerPage(page);
+        register.goto();
+        await register.fillRequiredFieldsExcept('lastname');
+    })
+
+    test('[AQAPRACT-514]: Register with max Last name length (255 characters)', async({page}) =>{
         
     })
 })
