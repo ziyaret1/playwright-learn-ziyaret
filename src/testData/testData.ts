24b48840aaf93199fa5107116d0958769fd0b023
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-09-19 15:48:08 +0400

    Completed

--- src/testData/testData.ts
+++ src/testData/testData.ts
@@ -3,8 +3,15 @@ export enum TestData {
   LASTNAME = "TestLastName",
   PASSWORD = "Test12345678",
   DATE_OF_BIRTH = "2000-01-01"
-};
+}; 
 
 export const generateUniqueEmail = (): string =>  {    //for make email address unique (not added testdata because it is not fixed)
 return `testzi${Date.now()}@mail.com`;
 } 
+
+export enum InvalidEmail {
+  SIMPLE = "Abc",
+  DOUBLE_AT = "Abc@abc@abc",
+  SPACE_IN_LOCAL = "Abc abc@abc",
+  INVALID_CHAR = "dsf()ds@ds"
+}
