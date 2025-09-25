3e39ea6df4dacecc2fdc9b3b65d184307610ee9b
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-09-25 18:51:08 +0400

    Revert "Merge pull request #11 from ziyaret1/ziyaret/userProfile/firstname"
    This reverts commit 2d5adf82346d039b1cef08cde7497c7e92524962, reversing
    changes made to f5c7429c47736880347b6e1f3d7777362905264d.

--- src/pages/userProfilePage.ts
+++ src/pages/userProfilePage.ts
@@ -1,156 +1,168 @@
 import { expect, type Page, type Locator } from "@playwright/test";
 import { BasePage } from "./basePage";
 import { PageUrls } from "../testData/testData";
+
 export class UserProfilePage extends BasePage {
-    readonly logoHeader: Locator;
-    readonly userPhoto: Locator;
-    readonly userName: Locator;
-    readonly position: Locator;
-    readonly email: Locator;
-    readonly technologies: Locator;
-    readonly dateOfBirth: Locator;
-    readonly editButton: Locator;
-    readonly aqaPracticeDropdown: Locator;
-    readonly signOutButton: Locator;
-    readonly footerLogo: Locator;
-    readonly footerContact: Locator;
-    readonly footerPhone: Locator;
-    readonly footerEmail: Locator;
+  readonly logoHeader: Locator;
+  readonly userPhoto: Locator;
+  readonly userName: Locator;
+  readonly position: Locator;
+  readonly email: Locator;
+  readonly technologies: Locator;
+  readonly dateOfBirth: Locator;
+  readonly editButton: Locator;
+  readonly aqaPracticeDropdown: Locator;
+  readonly signOutButton: Locator;
+  readonly footerLogo: Locator;
+  readonly footerContact: Locator;
+  readonly footerPhone: Locator;
+  readonly footerEmail: Locator;
+  // Dropdown menu
+  readonly dropdownOptionSelect: Locator;
+  readonly dropdownOptionDragDrop: Locator;
+  readonly dropdownOptionActions: Locator;
+  // Edit Profile modal
+  readonly modalCloseButton: Locator;
+  readonly editPersonalInfoTitle: Locator;
+  readonly editPersonalInfoText: Locator;
+  readonly firstNameInput: Locator;
+  readonly lastNameInput: Locator;
+  readonly emailInput: Locator;
+  readonly emailInfo: Locator;
+  readonly dateOfBirthInput: Locator;
+  readonly cancelButton: Locator;
+  readonly saveButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    // Profile page
+    this.logoHeader = page.locator('img[alt="Logo"]');
+    this.userPhoto = page.locator('img[alt=""]').first();
+    this.userName = page.locator("h1");
+    this.position = page
+      .locator("div.flex.flex-col.grow >> text=Position")
+      .locator("xpath=following-sibling::div[1]");
+    this.email = page
+      .locator("div.flex.flex-col.grow >> text=E-mail")
+      .locator("xpath=following-sibling::div[1]");
+    this.technologies = page
+      .locator("div.flex.flex-col.grow >> text=Technologies")
+      .locator("xpath=following-sibling::div[1]");
+    this.dateOfBirth = page
+      .locator("div.flex.flex-col.grow >> text=Date of birth")
+      .locator("xpath=following-sibling::div[1]");
+    this.editButton = page.locator('img[alt="Edit"]');
+    this.aqaPracticeDropdown = page.locator('div:text("AQA Practice")');
+    this.signOutButton = page.locator('div:text("Sign Out")');
+    this.footerLogo = page.locator('img[alt="Company Logo"]');
+    this.footerContact = page.getByText("Contact us", { exact: true });
+    this.footerPhone = page.locator('img[alt="Phone"]').locator("xpath=following-sibling::div");
+    this.footerEmail = page.locator('img[alt="Email"]').locator("xpath=following-sibling::div");
     // Dropdown menu
-    readonly dropdownOptionSelect: Locator;
-    readonly dropdownOptionDragDrop: Locator;
-    readonly dropdownOptionActions: Locator;
+    this.dropdownOptionSelect = page.getByText("Select", { exact: true });
+    this.dropdownOptionDragDrop = page.getByText("Drag & Drop", {
+      exact: true,
+    });
+    this.dropdownOptionActions = page.getByText("Actions, Alerts & Iframes", {
+      exact: true,
+    });
     // Edit Profile modal
-    readonly modalCloseButton: Locator;
-    readonly editPersonalInfoTitle: Locator;
-    readonly editPersonalInfoText: Locator;
-    readonly firstNameInput: Locator;
-    readonly firstnameRequiredErrorMessage: Locator;
-    readonly lengthErrorMessage: Locator;
-    readonly lastNameInput: Locator;
-    readonly emailInput: Locator;
-    readonly emailInfo: Locator;
-    readonly dateOfBirthInput: Locator;
-    readonly cancelButton: Locator;
-    readonly saveButton: Locator;
-    constructor(page: Page) {
-        super(page);
-        // Profile page
-        this.logoHeader = page.locator('img[alt="Logo"]');
-        this.userPhoto = page.locator('img[alt=""]').first();
-        this.userName = page.locator("h1");
-        this.position = page
-            .locator("div.flex.flex-col.grow >> text=Position")
-            .locator("xpath=following-sibling::div[1]");
-        this.email = page
-            .locator("div.flex.flex-col.grow >> text=E-mail")
-            .locator("xpath=following-sibling::div[1]");
-        this.technologies = page
-            .locator("div.flex.flex-col.grow >> text=Technologies")
-            .locator("xpath=following-sibling::div[1]");
-        this.dateOfBirth = page
-            .locator("div.flex.flex-col.grow >> text=Date of birth")
-            .locator("xpath=following-sibling::div[1]");
-        this.editButton = page.locator('img[alt="Edit"]');
-        this.aqaPracticeDropdown = page.locator('div:text("AQA Practice")');
-        this.signOutButton = page.locator('div:text("Sign Out")');
-        this.footerLogo = page.locator('img[alt="Company Logo"]');
-        this.footerContact = page.getByText("Contact us", { exact: true });
-        this.footerPhone = page.locator('img[alt="Phone"]').locator("xpath=following-sibling::div");
-        this.footerEmail = page.locator('img[alt="Email"]').locator("xpath=following-sibling::div");
-        // Dropdown menu
-        this.dropdownOptionSelect = page.getByText("Select", { exact: true });
-        this.dropdownOptionDragDrop = page.getByText("Drag & Drop", {
-            exact: true,
-        });
-        this.dropdownOptionActions = page.getByText("Actions, Alerts & Iframes", {
-            exact: true,
-        });
-        // Edit Profile modal
-        this.modalCloseButton = page.locator('img[alt="Close"]');
-        this.editPersonalInfoTitle = page.getByRole("heading", {
-            name: "Edit personal information",
-        });
-        this.editPersonalInfoText = page.getByText(
-            "Please, provide your personal information in English.",
-        );
-        this.firstNameInput = page.locator('input[name="firstName"]');
-        this.firstnameRequiredErrorMessage = page.locator('span.text-rose-500:text("Required")');
-        this.lengthErrorMessage = page.getByText("The value length shouldn't exceed 255 symbols.");
-        this.lastNameInput = page.locator('input[name="lastName"]');
-        this.emailInput = page.locator('input[name="email"]');
-        this.emailInfo = page
-            .locator('div:has-text("E-mail")')
-            .locator('xpath=following-sibling::div[contains(@class, "text-black")]');
-        this.dateOfBirthInput = page.locator('input[name="dateOfBirth"]');
-        this.cancelButton = page.getByRole("button", { name: "Cancel" });
-        this.saveButton = page.getByRole("button", { name: "Save" });
-    }
-    //! Methods
-    async goto(): Promise<void> {
-        await super.goto(PageUrls.USER_PROFILE);
-    }
-    //! User Profile
-    async verifyProfileLayout(): Promise<void> {
-        await expect(this.logoHeader).toBeVisible();
-        await expect(this.userPhoto).toBeVisible();
-        await expect(this.userName).toBeVisible();
-        await expect(this.position).toBeVisible();
-        await expect(this.email).toBeVisible(); //error
-        await expect(this.technologies).toBeVisible();
-        await expect(this.dateOfBirth).toBeVisible();
-        await expect(this.editButton).toBeVisible();
-        await expect(this.aqaPracticeDropdown).toBeVisible();
-        await expect(this.signOutButton).toBeVisible();
-        await expect(this.footerLogo).toBeVisible();
-        await expect(this.footerContact).toBeVisible();
-        await expect(this.footerPhone).toBeVisible();
-        await expect(this.footerEmail).toBeVisible();
-    }
-    async signOutSuccessfully(): Promise<void> {
-        await this.signOutButton.click();
-        await expect(this.page).toHaveURL(PageUrls.SIGNIN);
-    }
-    async verifyDropdownOptions() {
-        await this.aqaPracticeDropdown.click();
-        await expect(this.dropdownOptionSelect).toBeVisible();
-        await expect(this.dropdownOptionDragDrop).toBeVisible();
-        await expect(this.dropdownOptionActions).toBeVisible();
-    }
-    //! Edit Profile modal
-    async verifyEditPersonalInfoModal(): Promise<void> {
-        await expect(this.editPersonalInfoTitle).toBeVisible();
-        await expect(this.editPersonalInfoText).toBeVisible();
-        await expect(this.modalCloseButton).toBeVisible();
-        await expect(this.firstNameInput).not.toHaveValue("");
-        await expect(this.lastNameInput).not.toHaveValue("");
-        await expect(this.emailInput).not.toHaveValue("");
-        await expect(this.dateOfBirthInput).not.toHaveValue("");
-        await expect(this.cancelButton).toBeVisible();
-        await expect(this.saveButton).toBeVisible();
-    }
-    async updateFirstname(value: string): Promise<void> {
-        await this.firstNameInput.fill(value);
-    }
-    async updateLastname(value: string): Promise<void> {
-        await this.lastNameInput.fill(value);
-    }
-    async updateEmail(value: string): Promise<void> {
-        await this.emailInput.fill(value);
-    }
-    async updateDateOfBirth(value: string): Promise<void> {
-        await this.dateOfBirthInput.fill(value);
-    }
-    async clickSave(): Promise<void> {
-        await this.saveButton.click();
-        await expect(this.editPersonalInfoTitle).toHaveCount(0);
-    }
-    async clickCancel(): Promise<void> {
-        await this.cancelButton.click();
-        await expect(this.editPersonalInfoTitle).toHaveCount(0);
-    }
-    async clickCloseModal(): Promise<void> {
-        await this.modalCloseButton.click();
-        await expect(this.editPersonalInfoTitle).toHaveCount(0);
-    }
+    this.modalCloseButton = page.locator('img[alt="Close"]');
+    this.editPersonalInfoTitle = page.getByRole("heading", {
+      name: "Edit personal information",
+    });
+    this.editPersonalInfoText = page.getByText(
+      "Please, provide your personal information in English."
+    );
+    this.firstNameInput = page.locator('input[name="firstName"]');
+    this.lastNameInput = page.locator('input[name="lastName"]');
+    this.emailInput = page.locator('input[name="email"]');
+    this.emailInfo = page
+      .locator('div:has-text("E-mail")')
+      .locator('xpath=following-sibling::div[contains(@class, "text-black")]');
+    this.dateOfBirthInput = page.locator('input[name="dateOfBirth"]');
+    this.cancelButton = page.getByRole("button", { name: "Cancel" });
+    this.saveButton = page.getByRole("button", { name: "Save" });
+  }
+
+  //! Methods
+  async goto(): Promise<void> {
+    await super.goto(PageUrls.USER_PROFILE);
+  }
+
+  //! User Profile
+  async verifyProfileLayout(): Promise<void> {
+    await expect(this.logoHeader).toBeVisible();
+    await expect(this.userPhoto).toBeVisible();
+    await expect(this.userName).toBeVisible();
+    await expect(this.position).toBeVisible();
+    await expect(this.email).toBeVisible(); //error
+    await expect(this.technologies).toBeVisible();
+    await expect(this.dateOfBirth).toBeVisible();
+    await expect(this.editButton).toBeVisible();
+    await expect(this.aqaPracticeDropdown).toBeVisible();
+    await expect(this.signOutButton).toBeVisible();
+    await expect(this.footerLogo).toBeVisible();
+    await expect(this.footerContact).toBeVisible();
+    await expect(this.footerPhone).toBeVisible();
+    await expect(this.footerEmail).toBeVisible();
+  }
+
+  async signOutSuccessfully(): Promise<void> {
+    await this.signOutButton.click();
+    await expect(this.page).toHaveURL(
+      PageUrls.SIGNIN
+    );
+  }
+
+  async verifyDropdownOptions() {
+    await this.aqaPracticeDropdown.click();
+    await expect(this.dropdownOptionSelect).toBeVisible();
+    await expect(this.dropdownOptionDragDrop).toBeVisible();
+    await expect(this.dropdownOptionActions).toBeVisible();
+  }
+
+  //! Edit Profile modal
+  async verifyEditPersonalInfoModal(): Promise<void> {
+    await expect(this.editPersonalInfoTitle).toBeVisible();
+    await expect(this.editPersonalInfoText).toBeVisible();
+    await expect(this.modalCloseButton).toBeVisible();
+    await expect(this.firstNameInput).not.toHaveValue("");
+    await expect(this.lastNameInput).not.toHaveValue("");
+    await expect(this.emailInput).not.toHaveValue("");
+    await expect(this.dateOfBirthInput).not.toHaveValue("");
+    await expect(this.cancelButton).toBeVisible();
+    await expect(this.saveButton).toBeVisible();
+  }
+
+  async updateFirstname(value: string): Promise<void> {
+    await this.firstNameInput.fill(value);
+  }
+
+  async updateLastname(value: string): Promise<void> {
+    await this.lastNameInput.fill(value);
+  }
+
+  async updateEmail(value: string): Promise<void> {
+    await this.emailInput.fill(value);
+  }
+
+  async updateDateOfBirth(value: string): Promise<void> {
+    await this.dateOfBirthInput.fill(value);
+  }
+
+  async clickSave(): Promise<void> {
+    await this.saveButton.click();
+    await expect(this.editPersonalInfoTitle).toHaveCount(0);
+  }
+
+  async clickCancel(): Promise<void> {
+    await this.cancelButton.click();
+    await expect(this.editPersonalInfoTitle).toHaveCount(0);
+  }
+
+  async clickCloseModal(): Promise<void> {
+    await this.modalCloseButton.click();
+    await expect(this.editPersonalInfoTitle).toHaveCount(0);
+  }
 }
--- tests/userProfile/userProfile.spec.ts
+++ tests/userProfile/userProfile.spec.ts
@@ -1,96 +1,97 @@
 import { expect } from "@playwright/test";
-import { test } from "../../src/fixtures/fixture_signIn";
+import { test } from '../../src/fixtures/fixture_signIn';
 import { PageUrls, TestDataSignin } from "../../src/testData/testData";
+
 //! User Profile Suite
 test.describe("User Profile Suite", async () => {
-    test.beforeEach(async ({ signInPage }) => {
-        await signInPage.goto();
-        await signInPage.fillEmailAddress(TestDataSignin.EMAIL);
-        await signInPage.fillPasswordInput(TestDataSignin.PASSWORD);
-        await signInPage.submit();
-    });
-    test('[AQAPRACT-545] Validation of "User profile" page layout', async ({ userProfilePage }) => {
-        await userProfilePage.verifyProfileLayout();
-    });
-    test("[AQAPRACT-546] Successful Sign Out", async ({ userProfilePage }) => {
-        await userProfilePage.signOutSuccessfully();
-    });
-    test('[AQAPRACT-547] "AQA Practice" dropdown options validation', async ({
-        userProfilePage,
-    }) => {
-        await userProfilePage.verifyDropdownOptions();
-    });
+  test.beforeEach(async ({ signInPage }) => {
+    await signInPage.goto();
+    await signInPage.fillEmailAddress(TestDataSignin.EMAIL);
+    await signInPage.fillPasswordInput(TestDataSignin.PASSWORD);
+    await signInPage.submit();
+  });
+
+  test('[AQAPRACT-545] Validation of "User profile" page layout', async ({userProfilePage}) => {
+    await userProfilePage.verifyProfileLayout();
+  });
+
+  test("[AQAPRACT-546] Successful Sign Out", async ({userProfilePage}) => {
+    await userProfilePage.signOutSuccessfully();
+  });
+
+  test('[AQAPRACT-547] "AQA Practice" dropdown options validation', async ({userProfilePage}) => {
+    await userProfilePage.verifyDropdownOptions();
+  });
 });
+
 //! Edit Profile Suite
 test.describe("Edit Profile Suite", async () => {
-    test.beforeEach(async ({ signInPage }) => {
-        await signInPage.goto();
-        await signInPage.fillEmailAddress(TestDataSignin.EMAIL);
-        await signInPage.fillPasswordInput(TestDataSignin.PASSWORD);
-        await signInPage.submit();
-    });
-    test('[AQAPRACT-548] "Edit personal information" flyout available', async ({
-        userProfilePage,
-    }) => {
-        await userProfilePage.editButton.click();
-        await userProfilePage.verifyEditPersonalInfoModal();
-    });
-    test("[AQAPRACT-549] Edit 'First name' on User profile flyout", async ({ userProfilePage }) => {
-        await userProfilePage.editButton.click();
-        const newName: string = "NewAQA";
-        await userProfilePage.updateFirstname(newName);
-        await userProfilePage.clickSave();
-        await expect(userProfilePage.userName).toContainText(newName);
-    });
-    test('[AQAPRACT-550] Edit "Last name" on User profile flyout', async ({ userProfilePage }) => {
-        await userProfilePage.editButton.click();
-        const newLastName = "Test";
-        await userProfilePage.updateLastname(newLastName);
-        await userProfilePage.clickSave();
-        await expect(userProfilePage.userName).toContainText(newLastName);
-    });
-    test('[AQAPRACT-551] Edit "Email" on User profile flyout', async ({ userProfilePage }) => {
-        await userProfilePage.editButton.click();
-        const newEmail: string = "ziqa1@gmail.com";
-        await userProfilePage.emailInput.fill(newEmail);
-        // Input value check before save
-        await expect(userProfilePage.emailInput).toHaveValue(newEmail);
-        await expect(userProfilePage.emailInfo).toHaveText(newEmail);
-        await userProfilePage.clickSave();
-    });
-    test('[AQAPRACT-552] Edit "Date of Birth" on User profile flyout', async ({
-        userProfilePage,
-    }) => {
-        await userProfilePage.editButton.click();
-        const newDate = "01/01/2000";
-        await userProfilePage.updateDateOfBirth(newDate);
-        await userProfilePage.clickSave();
-        await expect(userProfilePage.dateOfBirth).toHaveText(newDate);
-    });
-    test("[AQAPRACT-553] Cancel editing the data on the flyout (after the data is edited)", async ({
-        userProfilePage,
-    }) => {
-        await userProfilePage.editButton.click();
-        await userProfilePage.updateFirstname("TemporaryName");
-        await userProfilePage.clickCancel();
-        await expect(userProfilePage.userName).not.toContainText("TemporaryName");
-    });
-    test("[AQAPRACT-554] Cancel editing the data on the flyout (without editing)", async ({
-        userProfilePage,
-    }) => {
-        await userProfilePage.editButton.click();
-        await userProfilePage.clickCancel();
-        await expect(userProfilePage.editPersonalInfoTitle).toHaveCount(0);
-        await expect(userProfilePage.getPage()).toHaveURL(PageUrls.USER_PROFILE);
-    });
-    test('[AQAPRACT-555] Close "Edit personal information" flyout by "X" button', async ({
-        userProfilePage,
-    }) => {
-        await userProfilePage.editButton.click();
-        const originalFirstName = await userProfilePage.firstNameInput.inputValue();
-        await userProfilePage.updateFirstname("TemporaryName");
-        await userProfilePage.clickCloseModal();
-        await expect(userProfilePage.editPersonalInfoTitle).toHaveCount(0);
-        await expect(userProfilePage.userName).toContainText(originalFirstName);
-    });
+  test.beforeEach(async ({ signInPage }) => {
+    await signInPage.goto();
+    await signInPage.fillEmailAddress(TestDataSignin.EMAIL);
+    await signInPage.fillPasswordInput(TestDataSignin.PASSWORD);
+    await signInPage.submit();
+  });
+
+  test('[AQAPRACT-548] "Edit personal information" flyout available', async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    await userProfilePage.verifyEditPersonalInfoModal();
+  });
+
+  test("[AQAPRACT-549] Edit 'First name' on User profile flyout", async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    const newName: string = "NewAQA";
+    await userProfilePage.updateFirstname(newName);
+    await userProfilePage.clickSave();
+    await expect(userProfilePage.userName).toContainText(newName);
+  });
+
+  test('[AQAPRACT-550] Edit "Last name" on User profile flyout', async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    const newLastName = "Test";
+    await userProfilePage.updateLastname(newLastName);
+    await userProfilePage.clickSave();
+    await expect(userProfilePage.userName).toContainText(newLastName);
+  });
+
+  test('[AQAPRACT-551] Edit "Email" on User profile flyout', async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    const newEmail: string = "ziqa1@gmail.com";
+    await userProfilePage.emailInput.fill(newEmail);
+    // Input value check before save
+    await expect(userProfilePage.emailInput).toHaveValue(newEmail);
+    await expect(userProfilePage.emailInfo).toHaveText(newEmail);
+    await userProfilePage.clickSave();
+  });
+
+  test('[AQAPRACT-552] Edit "Date of Birth" on User profile flyout', async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    const newDate = "01/01/2000";
+    await userProfilePage.updateDateOfBirth(newDate);
+    await userProfilePage.clickSave();
+    await expect(userProfilePage.dateOfBirth).toHaveText(newDate);
+  });
+
+  test("[AQAPRACT-553] Cancel editing the data on the flyout (after the data is edited)", async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    await userProfilePage.updateFirstname("TemporaryName");
+    await userProfilePage.clickCancel();
+    await expect(userProfilePage.userName).not.toContainText("TemporaryName");
+  });
+
+  test("[AQAPRACT-554] Cancel editing the data on the flyout (without editing)", async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    await userProfilePage.clickCancel();
+    await expect(userProfilePage.editPersonalInfoTitle).toHaveCount(0);
+    await expect(userProfilePage.getPage()).toHaveURL(PageUrls.USER_PROFILE)
+  });
+
+  test('[AQAPRACT-555] Close "Edit personal information" flyout by "X" button', async ({userProfilePage}) => {
+    await userProfilePage.editButton.click();
+    const originalFirstName = await userProfilePage.firstNameInput.inputValue();
+    await userProfilePage.updateFirstname("TemporaryName");
+    await userProfilePage.clickCloseModal();
+    await expect(userProfilePage.editPersonalInfoTitle).toHaveCount(0);
+    await expect(userProfilePage.userName).toContainText(originalFirstName);
+  });
 });
