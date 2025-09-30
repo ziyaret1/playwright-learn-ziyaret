bcdf63eddb13d5055d62218e3bb36f58793fb604
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-09-30 15:14:22 +0400

    Allure/Pipeline setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
-  ['list'],
-  ['allure-playwright', { outputFolder: 'allure-results', detail: true }]
-],
+    ['list'],
+    ['allure-playwright']
+  ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
