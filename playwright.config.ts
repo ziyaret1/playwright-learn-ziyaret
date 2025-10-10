afd4771b666de97e2af75e553e69a07f4cfbbf68
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-10-10 17:41:04 +0400

    API/Courses Fixes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
-import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import path from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 export default defineConfig({
     testDir: './tests',
