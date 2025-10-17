0f66e87238f22d2e279ac86aed49695d32c4ac34
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-10-17 16:30:30 +0400

    API/Health Check Completed

--- tests/api/healthCheck.spec.ts
+++ tests/api/healthCheck.spec.ts
@@ -3,12 +3,12 @@ import { test } from '../../src/fixtures/api/baseApi_fixture';
 import { HealthCheckEndpoints, TestDataSignin } from '../../src/testData/testData';
 
 test.describe('API / Health Check Suite', () => {
-    test('[secured]', async ({ coursesApi }) => {
+    test('[AQAPRACT-612] Get healthcheck for /api/secured service', async ({ coursesApi }) => {
         await coursesApi.ensureUserSignedIn(TestDataSignin.EMAIL, TestDataSignin.PASSWORD);
         const response = await coursesApi.getHealtCheck(HealthCheckEndpoints.HEALTH_CHECK_SECURED, true);
         expect(response).toBe('OK');
     });
-    test('[public]', async ({ coursesApi }) => {
+    test('[AQAPRACT-613] Get healthcheck for /api/public service', async ({ coursesApi }) => {
         const response = await coursesApi.getHealtCheck(HealthCheckEndpoints.HEALTH_CHECK_PUBLIC, false);
         expect(response).toBe('OK');
     });
