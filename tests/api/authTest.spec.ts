3de03890c6062ce9dcd328581fc1ef01ef8c643a
Ziyaret Seferova <ziyaretseferova@gmail.com>
2025-10-17 10:38:42 +0400

    API/Auth Suite Completed

--- tests/api/authTest.spec.ts
+++ tests/api/authTest.spec.ts
@@ -3,16 +3,11 @@ import { test } from '../../src/fixtures/api/baseApi_fixture';
 import { generateUniqueEmail, TestData, TestDataSignin } from '../../src/testData/testData';
 
 test.describe('Auth Suite', () => {
-    let email: string;
-    test.beforeEach(() => {
-        email = generateUniqueEmail();
-    });
-
     test('[AQAPRACT-612] User registration with valid data', async ({ coursesApi }) => {
         const bodyData = {
             firstName: TestData.FIRSTNAME,
             lastName: TestData.LASTNAME,
-            email,
+            email: generateUniqueEmail(),
             dateOfBirth: TestData.DATE_OF_BIRTH,
             password: TestData.PASSWORD,
         };
